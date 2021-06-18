69913a886aa2b4e118edf5e74f44ccff0c972ca4
Ricardo Signes <rjbs@semiotic.systems>
2021-06-17 22:17:27 -0400

    code-review: move more state management to mate

--- lib/RJBS/CodeReview.pm
+++ lib/RJBS/CodeReview.pm
@@ -10,6 +10,7 @@ with 'CliM8::App';
 use experimental 'signatures';
 
 use JSON::MaybeXS ();
+use List::AllUtils qw(uniq);
 
 sub name { 'code-review' }
 
@@ -25,6 +26,8 @@ sub decode_json_res ($self, $res) {
   return $self->json->decode( $res->decoded_content(charset => undef) );
 }
 
+has github_id => (is => 'ro');
+
 has _state => (
   is => 'ro',
   lazy    => 1,
@@ -32,6 +35,86 @@ has _state => (
     YAML::XS::LoadFile('code-review.yaml');
   },
 );
+
+sub _mark_reviewed {
+  my ($self, $name, $message) = @_;
+  $self->_state->{$name}{'last-review'} = DateTime->now(time_zone => 'local')
+                                           ->format_cldr('yyyy-MM-dd');
+
+  $self->_commit_state($name, $message);
+}
+
+sub _commit_state {
+  my ($self, $project, $message) = @_;
+
+  my $state = $self->_state;
+
+  my %dump = map {; $_ => {
+    ($state->{$_}{home}   ? (home => $state->{$_}{home})     : ()),
+    ($state->{$_}{review} ? (review => $state->{$_}{review}) : ()),
+    ($state->{$_}{'last-review'}
+    ? ('last-review' => $state->{$_}{'last-review'})
+    : ()),
+    ($state->{$_}{'review-every'}
+    ? ('review-every' => $state->{$_}{'review-every'})
+    : ()),
+  } } uniq(@main::projects, keys $state->%*);
+
+  YAML::XS::DumpFile('code-review.yaml', \%dump);
+
+  system(qw(git add code-review.yaml)) and die "git-add failed\n";
+  my $msg = $message
+          || ($project ? "did review of $project"
+                       : "rebuilt code-review state file");
+
+  my $gh_user = $self->github_id;
+  open my $mkdn, '>', 'code-review.mkdn' or die "can't open mkdn: $!";
+
+  print {$mkdn} <<END_HEADER;
+This file is computer-generated for humans to read.  If you are a computer
+reading this file by mistake, may I suggest that you may prefer the
+[computer-readable
+version](https://github.com/$gh_user/misc/blob/master/code-review.yaml) instead.
+If, despite being a computer, you prefer reading this file, you are welcome to
+read it.  Be advised, though, that its format may change in the future.
+
+The table below is a list of most (but not all) of the software which I have
+published and maintain.  Most of these projects are CPAN distributions.  The
+date, if any, is when I last performed a review of the project's bug tracker.
+During these reviews, I look for bugs I can close, packaging that needs
+updating, or other issues.  A review does not necessarily close all the open
+issues with a project.
+
+Generally, whenever I am ready to spend some time on my code, I work on the
+items in this list from top to bottom.  Once I've worked on an item, it moves
+to the bottom of the list.
+
+You can read [the program that generates this
+file](https://github.com/$gh_user/misc/blob/master/code-review) if you like.
+
+| PROJECT NAME                            | LAST REVIEW
+| --------------------------------------- | -------------
+END_HEADER
+
+  printf {$mkdn} "| %-40s| %s\n", $_, $state->{$_}{'last-review'} // '-'
+    for sort {
+      ($state->{$a}{'last-review'} // '0') cmp ($state->{$b}{'last-review'} // 0)
+      ||
+      fc $a cmp fc $b } @main::projects;
+
+  close $mkdn or die "error closing mkdn: $!";
+
+  system(qw(git add), <code-review.*>) and die "git-add failed\n";
+
+  my $lines = `git diff --staged`;
+  chomp $lines;
+  unless (length $lines) {
+    say "No changes to commit";
+    return;
+  }
+
+  system(qw(git commit -m), $msg)  and die "git-commit failed\n";
+}
 
 my %ACTIVITY = (
   boot    => 'RJBS::CodeReview::Activity::Boot',
@@ -224,7 +307,7 @@ package RJBS::CodeReview::Activity::Review {
 
       my $name = $project->{id};
       matesay "$name - No problems!  Great, moving on!";
-      main::mark_reviewed($name, "reviewed $name, no problems");
+      $self->app->_mark_reviewed($name, "reviewed $name, no problems");
 
       last PROJECT unless $self->queue->maybe_next;
     }
@@ -250,7 +333,7 @@ package RJBS::CodeReview::Activity::Review {
       $self->assert_queue_not_empty;
       my $project = $self->queue->get_current;
 
-      main::mark_reviewed($project->{id});
+      $self->app->_mark_reviewed($project->{id});
 
       okaysay("Cool, $project->{id} has been reviewed!");
 
